69691301cd0af6f1f38b026ca2d724b23f69ac2b
konopkrz <78698584+konopkrz@users.noreply.github.com>
2023-02-22 18:00:34 +0100

    25. Model seed

--- MyBoards/Entities/MyBoardsContext.cs
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -96,6 +96,11 @@ namespace MyBoards.Entities
                 .WithOne(a => a.User)
                 .HasForeignKey<Address>(a => a.UserId);
 
+            modelBuilder.Entity<WorkItemState>()
+                .HasData(new WorkItemState() { Id = 1, Value = "To do" },
+                         new WorkItemState() { Id = 2, Value = "Doing" },
+                         new WorkItemState() { Id = 3, Value = "Done" }
+                        );
         }
     }
 }
