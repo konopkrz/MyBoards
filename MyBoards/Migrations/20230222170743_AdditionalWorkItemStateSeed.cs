fc6af9de2b129f27d246533f3f94cd040015b9a6
konopkrz <78698584+konopkrz@users.noreply.github.com>
2023-02-22 18:31:38 +0100

    26. Ręczne dostosywanie migracji

---
+++ MyBoards/Migrations/20230222170743_AdditionalWorkItemStateSeed.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MyBoards.Entities;
+
+#nullable disable
+
+namespace MyBoards.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdditionalWorkItemStateSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(table: "WorkItemsStates", column: "Value", value: "On hold");
+            migrationBuilder.InsertData(table: "WorkItemsStates", column: "Value", value: "Rejected");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(table: "WorkItemsStates", keyColumn: "Value", keyValue: "Rejected");
+            migrationBuilder.DeleteData(table: "WorkItemsStates", keyColumn: "Value", keyValue: "On hold");
+
+        }
+    }
+}
