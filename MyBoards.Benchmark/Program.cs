618ea0ea93102faa0a29bb4d4b70a7f1b07b2721
konopkrz <78698584+konopkrz@users.noreply.github.com>
2023-02-27 14:22:08 +0100

    38. Change Tracker i jego wpływ na wydajność

---
+++ MyBoards.Benchmark/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace MyBoards.Benchmark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            BenchmarkRunner.Run<TrackingBenchmark>();
+        }
+    }
+}
