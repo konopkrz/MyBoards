70a801783cab96489f9ed207e2165ae30d932a40
konopkrz <78698584+konopkrz@users.noreply.github.com>
2023-02-28 13:33:49 +0100

    43. Lazy loading - Moja opinia - cofanie zmian

--- MyBoards/Entities/WorkItem.cs
+++ MyBoards/Entities/WorkItem.cs
@@ -33,13 +33,13 @@ namespace MyBoards.Entities
 
         public int Priority { get; set; }
 
-        public virtual List<Comment> Comments { get; set; } = new List<Comment>();
+        public List<Comment> Comments { get; set; } = new List<Comment>();
 
-        public virtual User Author { get; set; }
+        public User Author { get; set; }
 
         public Guid AuthorId { get; set; }
 
-        public virtual List<Tag> Tags { get; set; }
+        public List<Tag> Tags { get; set; }
 
 
     }
