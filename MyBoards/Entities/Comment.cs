4d21e20cb980373b89cdd3470396a8ef961cf7eb
konopkrz <78698584+konopkrz@users.noreply.github.com>
2023-02-28 13:34:45 +0100

    Revert "43. Lazy loading - Moja opinia - cofanie zmian"
    This reverts commit 70a801783cab96489f9ed207e2165ae30d932a40.

--- MyBoards/Entities/Comment.cs
+++ MyBoards/Entities/Comment.cs
@@ -8,11 +8,11 @@
         public DateTime CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
 
-        public WorkItem WorkItem { get; set; }
+        public virtual WorkItem WorkItem { get; set; }
 
         public int WorkItemId { get; set; }
 
-        public  User Author { get; set; }
+        public virtual User Author { get; set; }
 
         public Guid AuthorId { get; set; }
         
